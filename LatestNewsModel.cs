81e49b8d8969111e5826378564058c1104dbb6a2
Funmbi <bammieyour@gmail.com>
2022-01-03 23:02:55 +0100

    Add project files.

---
+++ LatestNewsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsApp2
+{
+    public class LatestNewsModel
+    {
+        public class SearchResponseModel
+        {
+            public string status { get; set; }
+            public int totalResults { get; set; }
+            public List<resultsObj> results { get; set; }
+        }
+        public class resultsObj
+        {
+            public string title { get; set; }
+            public string link { get; set; }
+            public object keywords { get; set; }
+            public List<string> creator { get; set; }
+            public object video_url { get; set; }
+            public string description { get; set; }
+            public object content { get; set; }
+            public string pubDate { get; set; }
+            public string image_url { get; set; }
+            public string source_id { get; set; }
+        }
+    }
+}
+}
---
+++ Models/LatestNewsModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsApp2.Models
+{
+    public class LatestNewsModel
+    {
+
+        public string status { get; set; }
+        public int totalResults { get; set; }
+        public List<resultsObj> results { get; set; }
+        public int responseCode { get; set; }
+        public string responseDescription { get; set; }
+    }
+
+        public class resultsObj
+        {
+            public string title { get; set; }
+            public string link { get; set; }
+            public List<string> keywords { get; set; }
+            public List<string> creator { get; set; }
+            public string video_url { get; set; }
+            public string description { get; set; }
+            public string content { get; set; }
+            public string pubDate { get; set; }
+            public string image_url { get; set; }
+            public string source_id { get; set; }
+        }
+}
+
